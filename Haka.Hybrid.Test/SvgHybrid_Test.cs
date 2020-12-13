62226661701e9a872549250800dd6258d1f0fa67
Robin Zevallos <robin.zevallos@gestionysistemas.com>
2020-12-13 02:11:02 -0500

    SvgHybrid test

---
+++ Haka.Hybrid.Test/SvgHybrid_Test.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Haka.Hybrid.Test
+{
+    public class SvgHybrid_Test : BaseTest
+    {
+        [Fact]
+        public void MockRender()
+        {
+            var svgHybrid = new SvgHybrid
+            {
+                Source = "spinner.svg"
+            };
+
+            Assert.NotNull(svgHybrid);
+        }
+    }
+}
